8336026e84413e2cb7ff21162b58a558938736b2
jribezzo <116099453+jribezzo@users.noreply.github.com>
2023-06-25 23:27:06 -0400

    aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5000/music/api',
+  apiBaseUrl: 'http://Music-env.eba-2k2pdxrt.us-east-2.elasticbeanstalk/music/api',
   spotifyClient: '254184c7d88442afa6faa5ef8bba5179',
   spotifySecret: '63dccd86ff4f4aa18d9c479ade29cc77'
 };
